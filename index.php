3c695451930191de13e415267d92230c35d70c6d
mtihila <114795938+mtihila@users.noreply.github.com>
2022-10-01 12:45:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>F&Q-Portal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <style type="text/css">
+      body img{
+        width: 100%;
+      }
+      .container{
+        display: flex;
+        justify-content: space-around;
+      }
+      
+      .coll-1 iframe{
+        width: 380px;
+        height: 400px;
+        border: 1px solid black;
+      }
+    </style>
+  </head>
+  <body>
+    <img src="images/banner.png" class="img-fluid" alt="...">
+    <div class="container">
+      <div class="coll-1">
+        <img src="images/mail.png">
+      </div>
+      <div class="coll-1">
+        <iframe src='https://webchat.botframework.com/embed/qnamakerforcowin-bot?s=OdzZZ7CpRk0.BobIIhWmNk40uHmVvspcCb5KTTcpuHxsGu6V4PC2mh8'></iframe>
+
+      </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
